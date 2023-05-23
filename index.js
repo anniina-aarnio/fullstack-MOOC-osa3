3da92b3528a22d0be3fc35a7e2829d33e5dfae5f
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-05-23 13:03:51 +0300

    added api/persons/:id

--- index.js
+++ index.js
@@ -38,5 +38,15 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => {
+    console.log(person.id, typeof person.id, id, typeof id, person.id === id);
+    return person.id === id;
+  });
+  console.log(person);
+  res.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
